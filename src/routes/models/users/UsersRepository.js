3e2a0d16e8e298add1559d3e8ec747e848b54a42
Flavia Mendes <flavinharamendes@gmail.com>
2024-10-31 16:46:19 -0300

    feat: create get user by id method

--- src/routes/models/users/UsersRepository.js
+++ src/routes/models/users/UsersRepository.js
@@ -1,19 +1,28 @@
-import User from './User.js';
+import User from "./User.js";
 
 class UsersRepository {
-  constructor() {
+constructor() {
     this.users = [];
-  }
+}
 
-  getAllUsers() {
+getAllUsers() {
     return this.users;
-  }
-  addUser (name, email, password) {
+}
+addUser(name, email, password) {
     const newUser = new User(name, email, password);
     this.users.push(newUser);
 
     return newUser;
-  }
 }
 
+getUserById(id) {
+    const user = this.users.find((u) => u.id == id);
+
+    if (!user) {
+    return null;
+    }
+
+    return user;
+}
+}
 export default UsersRepository;
